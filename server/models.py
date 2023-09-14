9b8d90c58ceaca9b2cd58784fa3524ac9960a5d3
Theodore Smith <tjs7321@gmail.com>
2023-09-14 05:48:41 -0400

    done

--- server/models.py
+++ server/models.py
@@ -11,13 +11,26 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    animals = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = False
+    animals = db.relationship('Animal', backref='enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    
+    # zookeeper = db.relationship('Zookeeper', back_populates='animals')
+    # enclosure = db.relationship('Enclosure', back_populates='animals')
